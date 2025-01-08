3ad6b177e1596cbceab0677c8b77e3e49a80e56d
ezequiel cabrera <92662432+zekecabre6@users.noreply.github.com>
2025-01-08 00:16:24 -0300

    Segundo commit7

--- src/data/designs.js
+++ src/data/designs.js
@@ -2,17 +2,17 @@ export const designs = [{
   id: 5,
   name: "Design 5",
   images: [
-    "https://i.pinimg.com/736x/c2/a0/86/c2a0863787fe53b50707ec509f98eebe.jpg", // Blanco
-    "https://i.pinimg.com/736x/17/13/76/1713760aa1c37b6a715cd4b4201c0055.jpg",  // Negro
-    "https://i.pinimg.com/736x/e3/ec/09/e3ec09e9803762a25124f363046d312b.jpg" //blue
+    "https://i.pinimg.com/1200x/c2/a0/86/c2a0863787fe53b50707ec509f98eebe.jpg", // Blanco
+    "https://i.pinimg.com/1200x/17/13/76/1713760aa1c37b6a715cd4b4201c0055.jpg",  // Negro
+    "https://i.pinimg.com/1200x/e3/ec/09/e3ec09e9803762a25124f363046d312b.jpg" //blue
   ],
   description: "Skeletons, con versiones en blanco, negro y azul."
 },{
   id: 4,
   name: "Design 4",
   images: [
-    "https://i.pinimg.com/736x/0b/c1/ca/0bc1cae60a3709d015fe1e75b7f031a7.jpg", // Blanco
-    "https://i.pinimg.com/736x/99/fb/cb/99fbcb641bd0aeb1d3ea6066c5cc46e4.jpg"  // Negro
+    "https://i.pinimg.com/1200x/0b/c1/ca/0bc1cae60a3709d015fe1e75b7f031a7.jpg", // Blanco
+    "https://i.pinimg.com/1200x/99/fb/cb/99fbcb641bd0aeb1d3ea6066c5cc46e4.jpg"  // Negro
   ],
   description: "La muerte acecha, con versiones en blanco y negro."
 },
@@ -20,8 +20,8 @@ export const designs = [{
     id: 1,
     name: "Design 1",
     images: [
-      "https://i.pinimg.com/736x/a1/57/05/a15705c91bde9a96e846eead71bb1f06.jpg", // Blanco
-      "https://i.pinimg.com/736x/1d/80/14/1d8014153b17ef89d63ed62592f1080b.jpg"  // Negro
+      "https://i.pinimg.com/1200x/a1/57/05/a15705c91bde9a96e846eead71bb1f06.jpg", // Blanco
+      "https://i.pinimg.com/1200x/1d/80/14/1d8014153b17ef89d63ed62592f1080b.jpg"  // Negro
     ],
     description: "Este es el primer diseño, con versiones en blanco y negro."
   },
@@ -29,8 +29,8 @@ export const designs = [{
     id: 2,
     name: "Design 2",
     images: [
-      "https://i.pinimg.com/736x/4f/19/8d/4f198d7913bb75c44310bf874e98db39.jpg", // Blanco
-      "https://i.pinimg.com/736x/63/3b/02/633b025b20de6a547374c0470dd8ecef.jpg"  // Negro
+      "https://i.pinimg.com/1200x/4f/19/8d/4f198d7913bb75c44310bf874e98db39.jpg", // Blanco
+      "https://i.pinimg.com/1200x/63/3b/02/633b025b20de6a547374c0470dd8ecef.jpg"  // Negro
     ],
     description: "Diseño elegante y moderno."
   },
@@ -38,8 +38,8 @@ export const designs = [{
     id: 3,
     name: "Design 3",
     images: [
-      "https://i.pinimg.com/736x/67/2d/23/672d234710447ea5e1a4dca20953acfb.jpg", // Blanco
-      "https://i.pinimg.com/736x/ca/af/f7/caaff75a1de2feaaaf91b5abfa6cf4e4.jpg"  // Color
+      "https://i.pinimg.com/1200x/67/2d/23/672d234710447ea5e1a4dca20953acfb.jpg", // Blanco
+      "https://i.pinimg.com/1200x/ca/af/f7/caaff75a1de2feaaaf91b5abfa6cf4e4.jpg"  // Color
     ],
     description: "Diseño colorido y vibrante para los que buscan algo único."
   }
--- src/data/products.js
+++ src/data/products.js
@@ -4,23 +4,23 @@ export const products = [
     name: "skeletons",
     price: "¿$?",  // Solo símbolo sin número
     colors: ["white", "black","white/b", "cream/b"],
-    mainImage: "https://i.pinimg.com/736x/68/e8/3c/68e83cd7a976be2d10433d5b4cc21f82.jpg",
+    mainImage: "https://i.pinimg.com/1200x/68/e8/3c/68e83cd7a976be2d10433d5b4cc21f82.jpg",
     designImages: [
-      "https://i.pinimg.com/736x/68/e8/3c/68e83cd7a976be2d10433d5b4cc21f82.jpg",
-      "https://i.pinimg.com/736x/91/de/e5/91dee5a7e587b64b3a1a533b2847a1c2.jpg",
-      "https://i.pinimg.com/736x/00/a9/97/00a9974577dbd3e1b13adbcbe6005968.jpg",
-      "https://i.pinimg.com/736x/d5/9b/34/d59b342adb857e7f182a1b5152fca4bc.jpg",
+      "https://i.pinimg.com/1200x/68/e8/3c/68e83cd7a976be2d10433d5b4cc21f82.jpg",
+      "https://i.pinimg.com/1200x/91/de/e5/91dee5a7e587b64b3a1a533b2847a1c2.jpg",
+      "https://i.pinimg.com/1200x/00/a9/97/00a9974577dbd3e1b13adbcbe6005968.jpg",
+      "https://i.pinimg.com/1200x/d5/9b/34/d59b342adb857e7f182a1b5152fca4bc.jpg",
     ],
   },{
     id: 4,
     name: "Death",
     price: "¿$?",  // Solo símbolo sin número
     colors: ["white", "black", "cream"],
-    mainImage: "https://i.pinimg.com/736x/fc/6d/4d/fc6d4d31e9987c973483739f54ad45e5.jpg",
+    mainImage: "https://i.pinimg.com/1200x/fc/6d/4d/fc6d4d31e9987c973483739f54ad45e5.jpg",
     designImages: [
-      "https://i.pinimg.com/736x/fc/6d/4d/fc6d4d31e9987c973483739f54ad45e5.jpg",
-      "https://i.pinimg.com/736x/30/61/d5/3061d5b7c3df74a13ee061694a87934d.jpg",
-      "https://i.pinimg.com/736x/d9/51/4e/d9514e3756704ccd409576735fea7910.jpg",
+      "https://i.pinimg.com/1200x/fc/6d/4d/fc6d4d31e9987c973483739f54ad45e5.jpg",
+      "https://i.pinimg.com/1200x/30/61/d5/3061d5b7c3df74a13ee061694a87934d.jpg",
+      "https://i.pinimg.com/1200x/d9/51/4e/d9514e3756704ccd409576735fea7910.jpg",
     ],
   },
     {
@@ -28,11 +28,11 @@ export const products = [
       name: "skull",
       price: "¿$?",  // Solo símbolo sin número
       colors: ["white", "black", "cream"],
-      mainImage: "https://i.pinimg.com/736x/9d/b5/84/9db5848040098c8fb5acef046c37cbb3.jpg",
+      mainImage: "https://i.pinimg.com/1200x/9d/b5/84/9db5848040098c8fb5acef046c37cbb3.jpg",
       designImages: [
-        "https://i.pinimg.com/736x/9d/b5/84/9db5848040098c8fb5acef046c37cbb3.jpg",
-        "https://i.pinimg.com/736x/87/75/f2/8775f2bbfa5f2d3e4da631bc02c5b446.jpg",
-        "https://i.pinimg.com/736x/1e/8e/69/1e8e69010c77bb13c18700ee5b5bcfd5.jpg",
+        "https://i.pinimg.com/1200x/9d/b5/84/9db5848040098c8fb5acef046c37cbb3.jpg",
+        "https://i.pinimg.com/1200x/87/75/f2/8775f2bbfa5f2d3e4da631bc02c5b446.jpg",
+        "https://i.pinimg.com/1200x/1e/8e/69/1e8e69010c77bb13c18700ee5b5bcfd5.jpg",
       ],
     },
     {
@@ -40,10 +40,10 @@ export const products = [
       name: "Baby",
       price: "¿$?",  // Solo símbolo sin número
       colors: ["white", "black"],
-      mainImage: "https://i.pinimg.com/736x/33/57/21/3357211c38baa81a771e4f4a96a137c4.jpg",
+      mainImage: "https://i.pinimg.com/1200x/33/57/21/3357211c38baa81a771e4f4a96a137c4.jpg",
       designImages: [
-        "https://i.pinimg.com/736x/33/57/21/3357211c38baa81a771e4f4a96a137c4.jpg",
-        "https://i.pinimg.com/736x/37/53/28/37532804e07120f2326dabddc56632f3.jpg",
+        "https://i.pinimg.com/1200x/33/57/21/3357211c38baa81a771e4f4a96a137c4.jpg",
+        "https://i.pinimg.com/1200x/37/53/28/37532804e07120f2326dabddc56632f3.jpg",
       ],
     },
     {
@@ -51,10 +51,10 @@ export const products = [
       name: "Cutface",
       price: "¿$?",  // Solo símbolo sin número
       colors: ["white", "black"],
-      mainImage: "https://i.pinimg.com/736x/e0/a4/5d/e0a45dccd1f6ae82c7c003e342d13655.jpg",
+      mainImage: "https://i.pinimg.com/1200x/e0/a4/5d/e0a45dccd1f6ae82c7c003e342d13655.jpg",
       designImages: [
-        "https://i.pinimg.com/736x/e0/a4/5d/e0a45dccd1f6ae82c7c003e342d13655.jpg",
-        "https://i.pinimg.com/736x/f4/c0/1e/f4c01e293c1d8384b4f46283c4b96657.jpg",
+        "https://i.pinimg.com/1200x/e0/a4/5d/e0a45dccd1f6ae82c7c003e342d13655.jpg",
+        "https://i.pinimg.com/1200x/f4/c0/1e/f4c01e293c1d8384b4f46283c4b96657.jpg",
       ],
     },
   ];
